3d38b5c7ca438b3430c8ee8d82429aa14038e41e
elmahdibenhadda <elmahdi.benhadda@gmail.com>
2015-05-23 20:43:00 +0200

    commit

--- src/testClass.java
+++ src/testClass.java
@@ -1,20 +1,22 @@
 import java.awt.Color;
 
-import datatype.Position;
-import Robot.Environnement;
-import Robot.Environnement.Robot;
 import Robot.Impl.EnvironnementImpl;
+import datatype.Position;
+
+
 
 
 public class testClass {
 
 	public static void main(String[] args) {
-			Environnement.Component environnement = new EnvironnementImpl().newComponent();
+		    Environnement.Component environnement = new EnvironnementImpl().newComponent();
 			Robot.Component r1 = environnement.create().createStandaloneRobot(Color.BLACK, new Position(0, 1));
 			Robot.Component r2 = environnement.create().createStandaloneRobot(Color.RED, new Position(8, 1));
 			
 			r1.decider().fakir();
 			r2.decider().fakir();
+			
+			// new Environnement.Impl.EnvironnementImpl().newComponent();
 	}
 
 }
